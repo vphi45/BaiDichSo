18f13131839bdb91094a5c8ec3e45c92e3731aa7
vphi45 <96299462+vphi45@users.noreply.github.com>
2022-04-04 19:26:51 +0700

    Add files via upload

---
+++ BaiDichSo/src/phi61134168/Client.java
@@ -0,0 +1,91 @@
+package phi61134168;
+
+import java.io.DataInputStream;
+
+import java.io.DataOutputStream;
+
+import java.io.IOException;
+
+import java.net.Socket;
+
+import java.util.Scanner;
+
+
+
+
+
+
+
+public class Client
+
+{
+
+
+
+   
+
+    public static void main(String[] args) throws IOException
+
+    {
+
+    // TODO code application logic here
+
+   
+
+        Socket mySocket = null;
+
+        DataOutputStream dos = null;
+
+        DataInputStream dis=null;
+
+       
+
+        try {
+
+            mySocket = new Socket("localhost", 8989);
+
+            dos = new DataOutputStream(mySocket.getOutputStream());
+
+            dis = new DataInputStream(mySocket.getInputStream());
+
+            Scanner input = new Scanner(System.in);
+
+            String s = null;
+
+            while(true)
+
+            {
+
+                System.out.print("\nNhập dữ liệu: ");
+
+                s =input.nextLine();
+
+                dos.writeUTF(s);
+
+               
+
+                String str=dis.readUTF();
+
+                System.out.print("Kết quả từ Server : "+str);
+
+            }
+
+        }
+
+        catch(Exception e) {
+
+            System.out.print("Ngắt kết nối");
+
+            dis.close();
+
+            dos.close();
+
+            mySocket.close();
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
---
+++ BaiDichSo/src/phi61134168/Server.java
@@ -0,0 +1,108 @@
+package phi61134168;
+
+import java.io.DataInputStream;
+
+import java.io.DataOutputStream;
+
+import java.io.IOException;
+
+import java.net.ServerSocket;
+
+import java.net.Socket;
+
+
+
+
+
+public class Server {
+
+private static ServerSocket serverSocket = null;
+
+public static void main(String[] args) throws IOException{
+
+// TODO code application logic here
+
+
+
+    DataOutputStream dos = null;
+
+    DataInputStream dis=null;
+
+    try {
+
+        serverSocket = new ServerSocket(8989);
+
+        System.out.print("Server đã được mở \n" );
+
+        Socket clientSocket = null;
+
+        clientSocket = serverSocket.accept();
+
+        dos=new DataOutputStream(clientSocket.getOutputStream());
+
+        dis=new DataInputStream(clientSocket.getInputStream());
+
+        String inline="";
+
+        while(true)
+
+        {
+
+            inline = dis.readUTF();
+
+            char ch[]=inline.toCharArray();
+
+            if(Character.isDigit(ch[0]))
+
+                {
+
+                int i=Integer.parseInt(inline);
+
+                switch(i)
+
+                {
+
+                    case 0:inline="Zero";break;
+
+                    case 1:inline="One";break;
+
+                    case 2:inline="Two";break;
+
+                    case 3:inline="Three";break;
+
+                    case 4:inline="Four";break;
+
+                    case 5:inline="Five";break;
+
+                    case 6:inline="Six";break;
+
+                    case 7:inline="Seven";break;
+
+                    case 8:inline="Eight";break;
+
+                    case 9:inline="Nine";break;
+
+                }
+
+                dos.writeUTF(inline);
+
+            }
+
+           
+    }
+
+    catch(Exception e) {
+
+        dos.close();
+
+        serverSocket.close();
+
+        dis.close();
+
+        System.out.print(e.getMessage());
+
+    }
+
+}
+
+}
